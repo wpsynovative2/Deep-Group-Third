3fb656e52729f3ac44c867fb3e9898534762afea
Brijendra Tiwari <brijendratiwari993@gmail.com>
2026-05-15 19:53:06 +0530

    Initial Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".", "");
   return {
     plugins: [react(), tailwindcss()],
+    base: "/Deep-Group-Third/",
     define: {
       "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
-      base: "/Deep-Group-Third/",
     },
     resolve: {
       alias: {
