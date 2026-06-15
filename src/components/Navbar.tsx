ca68fc50c909bfcbf622280096adccf035a4ef13
Brijendra Tiwari <brijendratiwari993@gmail.com>
2026-06-15 11:45:49 +0530

    Logo adedd

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { motion, AnimatePresence } from 'motion/react';
 import { Menu, X, Phone } from 'lucide-react';
 import { NAVIGATION_LINKS } from '../data/constants';
 import Button from './ui/Button';
+import logo from '../public/deep group Logo.png';
 
 export default function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -22,7 +23,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-6 flex justify-between items-center">
         <a href="#home" className="flex items-center gap-2">
           {/* Logo */}
-          <img src="https://deepgroup.co.in/wp-content/uploads/2025/11/cropped-deep-group-logo1.webp" alt="Deep Group Logo" className="h-10 w-auto object-contain" />
+          <img src={logo} alt="Deep Group Logo" className="h-15 w-auto object-contain" />
         </a>
 
         {/* Desktop Menu */}
